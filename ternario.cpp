29ce34e3a9452d73e26c215541611025e347e216
yennycabal <lyennycabanalope@gmail.com>
2026-05-19 22:34:57 -0500

    Add files via upload

---
+++ ternario.cpp
@@ -0,0 +1,15 @@
+/*utilizando ternario*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int A;
+    cout<<"Ingrese su edad por favor";
+    cin>>A;
+    (A<18)?cout<<"usted es menor":cout<<"usted es mayor";
+    return 0;
+    
+    
+
+    return 0;
+}
