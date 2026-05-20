f9d0587fd17380bbcf5cdea13fffa176c8988835
yennycabal <lyennycabanalope@gmail.com>
2026-05-19 23:44:22 -0500

    Add files via upload

---
+++ or.cpp
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+    switch de dias de la semana
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+int a,b;
+cout<<"ingrese a y b :";
+cin>>a>>b;
+cout<<a or b;
+
+    return 0;
+}
