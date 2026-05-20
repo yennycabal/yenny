4c2c8783a5fe347e4ba304ab2581963e44a72f6a
yennycabal <lyennycabanalope@gmail.com>
2026-05-19 22:47:56 -0500

    Add files via upload

---
+++ impar o par.cpp
@@ -0,0 +1,16 @@
+/*el programa define si es par o impar*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"ingresa dos numeros enteros";
+    cin>>a;
+    if(a%2){
+    cout<<"es impar";}
+    else{
+    cout<<"es par";}
+    
+
+    return 0;
+}
