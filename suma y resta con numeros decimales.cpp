48c3b19ad745b0f172fd981ee56df52d8fbde6a7
yennycabal <lyennycabanalope@gmail.com>
2026-05-20 00:11:32 -0500

    Add files via upload

---
+++ suma y resta con numeros decimales.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a=2.8;
+    int b =6;
+    float suma=a+b;
+    cout<<"respuesta (a+b) "<<suma<<endl;
+    float resta =a-b;
+    cout<<"respuesta (a-b)"<<resta<<endl;
+
+    return 0;
+}
