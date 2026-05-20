c14644a8a6f713d4f64e71bfb288d1775f0f31d8
yennycabal <lyennycabanalope@gmail.com>
2026-05-19 22:35:53 -0500

    Add files via upload

---
+++ comparandonumero de  galaxias.cpp
@@ -0,0 +1,16 @@
+/*comparando numero de galaxias*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"ingrese tres numeros enteros a,b y c ";
+    cin>>a>>b>>c;
+cout<<(a<b?"true ":"false")<<"";
+cout<<(c>a?"true ":"false")<<"";
+cout<<(a==b?"true ":"false")<<"";
+cout<<(a!=c?"true ":"false")<<"";
+cout<<(c<=b?"true ":"false")<<"";
+
+    return 0;
+}
