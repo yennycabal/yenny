c6bee6df2aa9916e76e61bf428ff7642c94d2adb
yennycabal <lyennycabanalope@gmail.com>
2026-05-19 23:04:21 -0500

    Add files via upload

---
+++ programa con and.cpp
@@ -0,0 +1,14 @@
+/*el programa de and o &&*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"ingrese a y b ";
+    cin>>a>>b;
+    cout<<"la respuesta es :";
+    cout<<(a&&b);
+    
+
+    return 0;
+}
